03872be6a271f254768051d8f1f573ac56d16c8b
fmaylinch <ferranmaylinch@gmail.com>
2020-09-24 14:15:22 +0200

    Function exercises

---
+++ exercicis/functions.rb
@@ -0,0 +1,66 @@
+# Exercisis
+
+# Notes:
+# - De vegades demano que la funció mostri (puts) o que retorni (return).
+# - De vegades he escrit part de l'exercisi ja. On posa "???" has d'escriure el teu codi.
+# - Per cada funció, escriu la definició (def) y també crida a la funció per comprovar que és correcte.
+
+
+# 1. Escriu una funció que mostri una frase motivadora.
+
+def display_motivational_sentence()
+    "???"
+end
+
+display_motivational_sentence()
+
+
+# 2. Escriu una funció que mostri el teu nom, com a paràmetre, i una frase motivadora.
+#    Per exemple: "Pere, aconseguiràs els teus objectius!"
+
+def display_motivational_sentence_for(name)
+    "???"
+end
+
+display_motivational_sentence_for("Pere")
+
+
+# 3. Escriu una funció que sumi 2 numeros (2 paràmetres) i mostri el resultat.
+
+
+# 4. Escriu una funció que sumi 2 numeros (2 paràmetres) i retorni el resultat.
+
+
+# 5. Escriu una funció que rep 2 números i retorna el més gran.
+
+def greater(a,b)
+    "???"
+end
+
+puts "El mes gran de 2 i 5 es: #{greater(2,5)}"
+
+
+# 5. Escriu una funció que rep 2 números i un booleà.
+#    Si el boolea es true, llavors retorna el número més gran, si no retorna el número més petit.
+
+
+# 6. Escriu una funció que repo 2 strings i els retorna junts (separats per un espai).
+
+def join_strings(s1, s2)
+    "???"
+end
+
+puts join_strings("hola", "que tal") # ha de mostrar "hola que tal"
+
+
+# 7. Escriu la funció factorial, que donat un número n retorna 1 * 2 * 3 * 4 * ... * n
+
+def factorial(n)
+    "???"
+end
+
+puts "Factorial de 5: #{factorial(5)}"
+
+
+# 8. Escriu una funció que, donat un número n, retorna un string així: "1, 2, 3, 4, ..., n"
+
